ac3ec2856cda8cb66cbbcbbd0436d0b49af48b4e
Kukuh Prihandoko Pratama <kukuh.pratama11@gmail.com>
2018-02-21 15:52:06 +0700

    lorem

--- latihan.php
+++ latihan.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Belajar 200</title>
   </head>
-  <body>W
-
+  <body>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus nemo ratione? Nobis optio ex maxime, praesentium assumenda quae laborum odio fugit exercitationem animi ducimus voluptate impedit, dolor in quam.</p>
   </body>
 </html>
