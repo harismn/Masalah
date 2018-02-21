620edda24e2504e775bb877b05803bfe9f2a3c35
harismn <harismannug@gmail.com>
2018-02-21 15:48:02 +0700

    makan

--- latihan.php
+++ latihan.php
@@ -5,6 +5,12 @@
     <title>Belajar 200</title>
   </head>
   <body>
+<<<<<<< HEAD
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus nemo ratione? Nobis optio ex maxime, praesentium assumenda quae laborum odio fugit exercitationem animi ducimus voluptate impedit, dolor in quam.</p>
+=======
+
+  <h1>Makan banyak pasti pintar</h1>
+
+>>>>>>> makan
   </body>
 </html>
